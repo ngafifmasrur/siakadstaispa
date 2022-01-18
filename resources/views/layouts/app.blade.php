3bc019fc6b8b3eeb34ad7f159f6846aea3373d08
Nursandi <sandinur157@gmail.com>
2022-01-18 09:13:23 +0700

    tambah label file selected input [type=file]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -291,6 +291,16 @@
 		</script>
 		@endif
 
+		<script>
+			$('.custom-file-input').on('change', function () {
+				let filename = $(this).val().split('\\').pop();
+				$(this)
+					.next('.custom-file-label')
+					.addClass('selected')
+					.html(filename);
+			});
+		</script>
+
         @stack('js')
 	</body>
 </html>
