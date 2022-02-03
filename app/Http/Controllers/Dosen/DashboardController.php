6af024569f33212100a68941b4daab8932e3a31e
dwiksurya <dwiks14@gmail.com>
2022-02-03 09:54:10 +0700

    dashboard dosen

--- app/Http/Controllers/Dosen/DashboardController.php
+++ app/Http/Controllers/Dosen/DashboardController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\m_jadwal;
 use App\Models\t_semester_mahasiswa;
+use App\Models\m_kelas_kuliah;
 use Carbon\Carbon, Auth;
 
 class DashboardController extends Controller
@@ -14,14 +15,14 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $jadwal_mengajar = m_jadwal::query()->where('id_dosen', Auth::user()->dosen->id_dosen)
-                            ->select('m_jadwal.*', 'm_mata_kuliah.kode_mata_kuliah', 'm_mata_kuliah.nama_mata_kuliah')
-                            ->join('m_mata_kuliah_aktif', 'm_mata_kuliah_aktif.id', 'm_jadwal.id_matkul_aktif')
-                            ->join('m_mata_kuliah', 'm_mata_kuliah.id_matkul', 'm_mata_kuliah_aktif.id_matkul')
-                            ->where('hari', Carbon::now()->isoFormat('dddd'))->withCount('krs');
+        // $jadwal_mengajar = m_jadwal::query()->where('id_dosen', Auth::user()->dosen->id_dosen)
+        //                     ->select('m_jadwal.*', 'm_mata_kuliah.kode_mata_kuliah', 'm_mata_kuliah.nama_mata_kuliah')
+        //                     ->join('m_mata_kuliah_aktif', 'm_mata_kuliah_aktif.id', 'm_jadwal.id_matkul_aktif')
+        //                     ->join('m_mata_kuliah', 'm_mata_kuliah.id_matkul', 'm_mata_kuliah_aktif.id_matkul')
+        //                     ->where('hari', Carbon::now()->isoFormat('dddd'))->withCount('krs');
         $total_jadwal_mengajar = m_jadwal::where('id_dosen', Auth::user()->dosen->id_dosen)->count();
         $total_mahasiswa_bimbingan = t_semester_mahasiswa::where('id_dosen_pembimbing', Auth::user()->dosen->id_dosen)->count();
-        
+        $jadwal_mengajar = m_kelas_kuliah::where('id_dosen', Auth::user()->id_dosen)->get();
         return view('dosen.dashboard', compact('total_jadwal_mengajar', 'total_mahasiswa_bimbingan', 'jadwal_mengajar'));
     }
 }
--- resources/views/dosen/dashboard.blade.php
+++ resources/views/dosen/dashboard.blade.php
@@ -17,78 +17,147 @@
     </ol>
 </div>
 
-<div class="col-xl-12 col-lg-12 col-md-12">
-    <div class="row">
-        <div class="col-xl-4 col-lg-6">
-            <div class="card overflow-hidden">
-                <div class="card-body pb-0">
-                    <div class="d-flex">
-                        <div>
-                            <p class="mb-1 text-uppercase">Total Jadwal Mengajar</p>
-                        <h2 class="fs-2 mb-2">{{ $total_jadwal_mengajar }}</h2>
-                        <a href="{{ route('dosen.jadwal_mengajar.index') }}" class="btn btn-secondary view-more mb-3">List Jadwal Mengajar</a>
-                        </div>
-                        <i class="fa fa-calendar fa-5x ml-auto fs-3 op4 text-danger"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-4 col-lg-6">
-            <div class="card overflow-hidden">
-                <div class="card-body pb-0">
-                    <div class="d-flex">
-                        <div>
-                            <p class="mb-1 text-uppercase">Total Mahasiswa Bimbingan</p>
-                            <h2 class="fs-2 mb-7">{{ $total_mahasiswa_bimbingan }}</h2>
-                        </div>
-                        <i class="fa fa-user fa-5x ml-auto fs-3 op4 text-warning"></i>
+<div class="row">
+    <div class="col-lg-3 col-md-12">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12">
+                <div class="card overflow-hidden">
+                    <div class="card-body pb-0">
+                        <div id="cal" class="mb-2"></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="col-lg-6 col-md-12">
+        <div class="card border-0">
+            <div class="card-header custom-header pb-0">
+                <div>
+                    <h3 class="card-title">Jadwal Mengajar Hari Ini</h3>
+                    <h6 class="card-subtitle">{{ Carbon\Carbon::now()->isoFormat('dddd, M Y')}}</h6>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    {{-- <table class="table table-bordered text-nowrap mb-0">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Mata Kuliah</th>
+                                <th>Kode MK</th>
+                                <th>Ruang</th>
+                                <th>Waktu</th>
+                                <th>Jumlah Peserta</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($jadwal_mengajar as $item)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->nama_mata_kuliah }}</td>
+                                    <td>{{ $item->kode_mata_kuliah }}</td>
+                                    <td>{{ $item->ruangan->nama_ruangan }}</td>
+                                    <td>{{ $item->jam_mulai.' - '.$item->jam_akhir }}</td>
+                                    <td><span class="badge badge-success">{{ $item->krs_count }}</span></td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6">
+                                        <p class="text-center">Tidak ada jadwal hari ini</p>
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table> --}}
+                    @foreach ($jadwal_mengajar as $item)
+                        <div class="card">
+                            <div class="card-status card-status-left bg-primary br-bl-7 br-tl-7"></div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-4 col-md-12">
+                                        <small class="block">{{ $item->nama_semester }}</small>
+                                        <strong  class="block">{{ $item->nama_mata_kuliah }}</strong>
+                                    </div>
+                                    <div class="col-lg-5 col-md-12">
+                                        <span class="block mb-1"><i class="fa fa-calendar mr-1"></i> {{ $item->nama_semester }}</span>
+                                        <span class="block mb-1"><i class="fa fa-bank mr-1"></i> {{ $item->nama_kelas_kuliah }}</span>
+                                        <span class="block mb-1"><i class="fa fa-book mr-1"></i> {{ $item->nama_program_studi }}</span>
 
-<div class="col-xl-12 col-lg-12 col-md-12">
-    <div class="card">
-        <div class="card-header custom-header pb-0">
-            <div>
-                <h3 class="card-title">Jadwal Mengajar Hari Ini</h3>
-                <h6 class="card-subtitle">{{ Carbon\Carbon::now()->isoFormat('dddd, M Y')}}</h6>
+                                    </div>
+                                    <div class="col-lg-3 col-md-12">
+                                        <button type="button" class="btn btn-sm btn-primary">
+                                            Perkuliahan
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered text-nowrap mb-0">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Mata Kuliah</th>
-                            <th>Kode MK</th>
-                            <th>Ruang</th>
-                            <th>Waktu</th>
-                            <th>Jumlah Peserta</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($jadwal_mengajar as $item)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $item->nama_mata_kuliah }}</td>
-                                <td>{{ $item->kode_mata_kuliah }}</td>
-                                <td>{{ $item->ruangan->nama_ruangan }}</td>
-                                <td>{{ $item->jam_mulai.' - '.$item->jam_akhir }}</td>
-                                <td><span class="badge badge-success">{{ $item->krs_count }}</span></td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="6">
-                                    <p class="text-center">Tidak ada jadwal hari ini</p>
-                                </td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+    </div>
+    
+    <div class="col-lg-3 col-md-12">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12">
+                <div class="card">
+                    <div class="card-header pb-0">
+                        <h3 class="card-title">Informasi Terbaru</h3>
+                    </div>
+                    <div class="card-body p-0 ">
+                        <div class="list-group list-lg-group list-group-flush">
+                            <a class="list-group-item list-group-item-action" href="#">
+                                <div class="media mt-0">
+                                    <img class="avatar-lg rounded-circle mr-3" src="{{ asset('landing_page/images/imp_logo.png') }}" alt="Image description">
+                                    <div class="media-body">
+                                        <div class="d-md-flex align-items-center">
+                                            <h4 class="mb-1">
+                                                Lorem isum dolor sit amet
+                                            </h4>
+                                        </div>
+    
+                                        <p class="mb-0">Itaque earum rerum hic tenetur a sapiente reiciendis voluptatibus.</p>
+                                        <small class="text-primary ml-md-auto"><i class="fe fe-calendar mr-1"></i> 26 Feb 2019</small>
+
+                                    </div>
+                                </div>
+                            </a>
+                            <a class="list-group-item list-group-item-action" href="#">
+                                <div class="media mt-0">
+                                    <img class="avatar-lg rounded-circle mr-3" src="{{ asset('landing_page/images/imp_logo.png') }}" alt="Image description">
+                                    <div class="media-body">
+                                        <div class="d-md-flex align-items-center">
+                                            <h4 class="mb-1">
+                                                Lorem isum dolor sit amet
+                                            </h4>
+                                        </div>
+    
+                                        <p class="mb-0">Itaque earum rerum hic tenetur a sapiente reiciendis voluptatibus.</p>
+                                        <small class="text-primary ml-md-auto"><i class="fe fe-calendar mr-1"></i> 26 Feb 2019</small>
+
+                                    </div>
+                                </div>
+                            </a>
+                            <a class="list-group-item list-group-item-action" href="#">
+                                <div class="media mt-0">
+                                    <img class="avatar-lg rounded-circle mr-3" src="{{ asset('landing_page/images/imp_logo.png') }}" alt="Image description">
+                                    <div class="media-body">
+                                        <div class="d-md-flex align-items-center">
+                                            <h4 class="mb-1">
+                                                Lorem isum dolor sit amet
+                                            </h4>br
+                                        </div>
+                                        <p class="mb-0">Itaque earum rerum hic tenetur a sapiente reiciendis voluptatibus.</p>
+                                        <small class="text-primary ml-md-auto"><i class="fe fe-calendar mr-1"></i> 26 Feb 2019</small>
+                                    </div>
+                                </div>
+                            </a>
+                            <hr class="mt-auto mb-auto">
+                            <a href="#" class="text-primary p-2">Lihat Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -96,6 +165,7 @@
 @endsection
 
 @push('css')
+    <link href="{{ asset('sparic/css/calendar.css') }}" rel="stylesheet" />
     <style>
         .view-more {
             /* border-radius: 10rem; */
@@ -104,5 +174,28 @@
             padding-top: 0.1em;
             padding-bottom: 0.1em;
         }
+
+        p, ul, ol {
+            list-style-type: none!important;
+            margin: 0!important;
+            padding: 0!important;
+        }
+
+        .calendar-hd {
+            padding: 0!important;
+            height: 30px;
+            line-height: 30px;
+        }
     </style>
+@endpush
+
+@push('js')
+    <!-- Default Calendar js-->
+    <script src="{{ asset('sparic/js/calendar.js') }}"></script>
+    <script>
+        $('#cal').calendar({
+            monthArray: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            weekArray: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+        });
+    </script>
 @endpush
