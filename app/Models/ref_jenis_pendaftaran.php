a6278aec8d73b61501bb51ca977200567922dbff
dwiksurya <dwiks14@gmail.com>
2022-02-03 16:18:43 +0700

    ref jenis daftar

--- app/Models/ref_jenis_pendaftaran.php
+++ app/Models/ref_jenis_pendaftaran.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Sushi\Sushi;
 
 class ref_jenis_pendaftaran extends Model
 {
-    use HasFactory;
+    use HasFactory, Sushi;
 
-    protected $table = 'ref_jenis_pendaftaran';
-    protected $guarded = [];
     protected $primaryKey = 'id_jenis_daftar';
+    protected $guarded = [];
+    public $incrementing = false;
+    protected $keyType = 'string';
 
+    public function getRows()
+    {
+        return GetDataFeeder('GetJenisPendaftaran');
+    }
 }
