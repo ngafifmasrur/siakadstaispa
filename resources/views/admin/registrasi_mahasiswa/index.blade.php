239d36ce877879c04d360848888f85b0ca8daf3a
dwiksurya <dwiks14@gmail.com>
2022-02-04 13:37:57 +0700

    #44

--- resources/views/admin/registrasi_mahasiswa/index.blade.php
+++ resources/views/admin/registrasi_mahasiswa/index.blade.php
@@ -7,6 +7,19 @@
     Registrasi Mahasiswa
 </x-header>
 
+<x-card>
+    <div class="row">
+        <div class="form-group col-lg-3">
+            <label for="prodi">Program Studi</label>
+            {!! Form::select('prodi', $prodi, null, ['class' => 'form-control', 'id' => 'prodi']) !!}
+        </div>
+        <div class="form-group col-lg-3">
+            <label for="periode">Periode Masuk</label>
+            {!! Form::select('periode', $periode, null, ['class' => 'form-control', 'id' => 'periode']) !!}
+        </div>
+    </div>
+</x-card>
+
 <x-card-table>
     <x-slot name="title">Data Registrasi Mahasiswa</x-slot>
     <x-slot name="button">
@@ -22,6 +35,10 @@
         ['title' => 'Periode Masuk', 'data' => 'periode', 'name' => 'periode'],
         ['title' => 'Program Studi', 'data' => 'prodi', 'name' => 'prodi', 'classname' => 'text-left'],
         ['title' => 'Aksi', 'data' => 'action', 'orderable' => 'false', 'searchable' => 'false'],
+    ]"
+    :filter="[
+        ['data' => 'id_prodi', 'value' => '$(`#prodi`).val()'],
+        ['data' => 'id_periode_masuk', 'value' => '$(`#periode`).val()']
     ]"
     />
 
@@ -112,6 +129,12 @@
 
 @push('js')
     <script>
+        $( document ).ready(function() {
+            $(document).on('change','#prodi, #periode',function(){
+                table.ajax.reload();
+            });
+        });
+    
         $('.add-form').on('click', function () {
             $('.modal-form').modal('show');
             $('.modal-form form')[0].reset();
