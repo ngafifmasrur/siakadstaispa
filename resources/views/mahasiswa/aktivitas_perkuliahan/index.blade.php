2215f9aec0f9316b779ca15b693c89e0c42aaa3e
dwi <dwi@impstudio.id>
2022-02-09 19:32:52 +0700

    #67

---
+++ resources/views/mahasiswa/aktivitas_perkuliahan/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title', 'Aktivitas Perkuliahan')
+
+@section('content')
+
+<x-header>
+    Aktivitas Perkuliahan
+</x-header>
+
+<x-card-table>
+    <x-slot name="title">Data Aktivitas Perkuliahan</x-slot>
+
+    <x-datatable 
+    :route="route('mahasiswa.aktivitas_perkuliahan.data_index')" 
+    :table="[
+        ['title' => 'No.', 'data' => 'DT_RowIndex', 'name' => 'DT_RowIndex', 'orderable' => 'false', 'searchable' => 'false', 'width' => '10'],                            
+        ['title' => 'Semester', 'data' => 'nama_semester', 'name' => 'jenis_aktivitas'],
+        ['title' => 'Status', 'data' => 'nama_status_mahasiswa', 'name' => 'nama_status_mahasiswa', 'classname' => 'text-center'],
+        ['title' => 'IPS', 'data' => 'ips', 'name' => 'ips'],
+        ['title' => 'IPK', 'data' => 'ipk', 'name' => 'ipk'],
+        ['title' => 'SKS Semester', 'data' => 'sks_semester', 'name' => 'sks_semester'],
+        ['title' => 'SKS Total', 'data' => 'sks_total', 'name' => 'sks_total'],
+    ]"
+    />
+
+</x-card-table>
+
+@endsection
