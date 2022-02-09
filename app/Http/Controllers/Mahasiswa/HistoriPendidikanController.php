f101a8db867e14e6b46b6953c86b7a3d86003cd8
dwi <dwi@impstudio.id>
2022-02-09 19:30:46 +0700

    #68

---
+++ app/Http/Controllers/Mahasiswa/HistoriPendidikanController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Mahasiswa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\{
+    t_riwayat_pendidikan_mahasiswa,
+    m_global_konfigurasi
+};
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Session, DB;
+
+class HistoriPendidikanController extends Controller
+{
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('mahasiswa.histori_pendidikan.index');
+    }
+
+    public function data_index(Request $request)
+    {
+        $user = Auth::user();
+        $semester = m_global_konfigurasi::first()->value('id_semester_aktif');
+
+        $query = t_riwayat_pendidikan_mahasiswa::setFilter([
+            'filter' => "id_mahasiswa='$user->id_mahasiswa' AND id_periode_masuk='$semester'"
+        ])->get();
+
+        return datatables()->of($query)
+            ->addIndexColumn()
+            // ->addColumn('action',function ($data) {
+           
+            //     $button = '<div class="btn-group" role="group" aria-label="Basic example">';
+    
+            //     $button .= view("components.button.default", [
+            //         'type' => 'button',
+            //         'tooltip' => 'Ubah',
+            //         'class' => 'btn btn-outline-primary btn-sm btn_edit',
+            //         "icon" => "fa fa-edit",
+            //         'attribute' => [
+            //             'data-id_jenis_aktivitas' => $data->id_jenis_aktivitas,
+            //             'data-id_prodi' => $data->id_prodi,
+            //             'data-id_semester' => $data->id_semester,
+            //             'data-judul' => $data->judul,
+            //             'data-keterangan' => $data->keterangan,
+            //             'data-lokasi' => $data->lokasi,
+            //             'data-sk_tugas' => $data->sk_tugas,
+            //         ],
+            //         "route" => route('admin.aktivitas.update', $data->id_aktivitas),
+            //     ]);
+    
+            //     $button .= view("components.button.default", [
+            //         'type' => 'button',
+            //         'tooltip' => 'Hapus',
+            //         'class' => 'btn btn-outline-danger btn-sm btn_delete',
+            //         "icon" => "fa fa-trash",
+            //         'attribute' => [
+            //             'data-text' => 'Anda yakin ingin menghapus data ini ?',
+            //         ],
+            //         "route" => route('admin.aktivitas.destroy', $data->id_aktivitas),
+            //     ]);
+    
+            //     $button .= '</div>';
+    
+            //     return $button;
+            // })
+            // ->rawColumns(['action'])
+            ->setRowAttr([
+                'style' => 'text-align: center',
+            ])
+            ->toJson();
+    }
+}
---
+++ resources/views/mahasiswa/histori_pendidikan/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title', 'Aktivitas Perkuliahan')
+
+@section('content')
+
+<x-header>
+    Histori Pendidikan
+</x-header>
+
+<x-card-table>
+    <x-slot name="title">Data Histori Pendidikan</x-slot>
+
+    <x-datatable 
+    :route="route('mahasiswa.histori_pendidikan.data_index')" 
+    :table="[
+        ['title' => 'No.', 'data' => 'DT_RowIndex', 'name' => 'DT_RowIndex', 'orderable' => 'false', 'searchable' => 'false', 'width' => '10'],                            
+        ['title' => 'NIM', 'data' => 'nim', 'name' => 'nim'],
+        ['title' => 'Jenis Pendaftaran', 'data' => 'nama_jenis_daftar', 'name' => 'nama_jenis_daftar', 'classname' => 'text-center'],
+        ['title' => 'Periode', 'data' => 'nama_periode_masuk', 'name' => 'nama_periode_masuk', 'classname' => 'text-left'],
+        ['title' => 'Tanggal Masuk', 'data' => 'tanggal_daftar', 'name' => 'tanggal_daftar'],
+        ['title' => 'Perguruan Tinggi', 'data' => 'nama_perguruan_tinggi', 'name' => 'nama_perguruan_tinggi', 'classname' => 'text-left'],
+        ['title' => 'Program Studi', 'data' => 'nama_program_studi', 'name' => 'nama_program_studi', 'classname' => 'text-left'],
+    ]"
+    />
+
+</x-card-table>
+
+@endsection
