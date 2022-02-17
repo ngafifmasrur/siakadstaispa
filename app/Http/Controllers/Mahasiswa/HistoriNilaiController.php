df6ebafc046dfb401a7e2fc037b42bd4acafe4d4
dwi <dwi@impstudio.id>
2022-02-17 14:59:36 +0700

    #63 Fix KHS

--- app/Http/Controllers/Mahasiswa/HistoriNilaiController.php
+++ app/Http/Controllers/Mahasiswa/HistoriNilaiController.php
@@ -37,7 +37,7 @@ class HistoriNilaiController extends Controller
 
         if(isset($riwayat_pendidikan)) {
             $query = t_riwayat_nilai_mahasiswa::setFilter([
-                'filter' => "id_registrasi_mahasiswa='$riwayat_pendidikan->id_registrasi_mahasiswa'"
+                'filter' => "id_registrasi_mahasiswa='$riwayat_pendidikan->id_registrasi_mahasiswa' AND id_periode='$request->periode'"
             ])->get();
 
             $query->map(function ($item){
@@ -65,43 +65,6 @@ class HistoriNilaiController extends Controller
             ->addColumn('total_nilai',function ($data) {
                 return $data->total_nilai;
             })
-            // ->addColumn('action',function ($data) {
-           
-            //     $button = '<div class="btn-group" role="group" aria-label="Basic example">';
-    
-            //     $button .= view("components.button.default", [
-            //         'type' => 'button',
-            //         'tooltip' => 'Ubah',
-            //         'class' => 'btn btn-outline-primary btn-sm btn_edit',
-            //         "icon" => "fa fa-edit",
-            //         'attribute' => [
-            //             'data-id_jenis_aktivitas' => $data->id_jenis_aktivitas,
-            //             'data-id_prodi' => $data->id_prodi,
-            //             'data-id_semester' => $data->id_semester,
-            //             'data-judul' => $data->judul,
-            //             'data-keterangan' => $data->keterangan,
-            //             'data-lokasi' => $data->lokasi,
-            //             'data-sk_tugas' => $data->sk_tugas,
-            //         ],
-            //         "route" => route('admin.aktivitas.update', $data->id_aktivitas),
-            //     ]);
-    
-            //     $button .= view("components.button.default", [
-            //         'type' => 'button',
-            //         'tooltip' => 'Hapus',
-            //         'class' => 'btn btn-outline-danger btn-sm btn_delete',
-            //         "icon" => "fa fa-trash",
-            //         'attribute' => [
-            //             'data-text' => 'Anda yakin ingin menghapus data ini ?',
-            //         ],
-            //         "route" => route('admin.aktivitas.destroy', $data->id_aktivitas),
-            //     ]);
-    
-            //     $button .= '</div>';
-    
-            //     return $button;
-            // })
-            // ->rawColumns(['action'])
             ->setRowAttr([
                 'style' => 'text-align: center',
             ])
@@ -122,16 +85,20 @@ class HistoriNilaiController extends Controller
 
         if(isset($riwayat_pendidikan)) {
             $nilai = t_riwayat_nilai_mahasiswa::setFilter([
-                'filter' => "id_registrasi_mahasiswa='$riwayat_pendidikan->id_registrasi_mahasiswa'"
+                'filter' => "id_registrasi_mahasiswa='$riwayat_pendidikan->id_registrasi_mahasiswa' AND id_periode='$request->periode'"
             ])->get();
             
             $nilai->map(function ($item){
                 $matkul = m_mata_kuliah::setFilter([
                     'filter' => "id_matkul='$item->id_matkul'"
                 ])->first();
+                $semester = m_semester::setFilter([
+                    'filter' => "id_semester='$item->id_periode'"
+                ])->first();
                 $item['kode_mata_kuliah'] = $matkul->kode_mata_kuliah;
                 $item['sks_mata_kuliah'] = $matkul->sks_mata_kuliah;
                 $item['total_nilai'] = $matkul->sks_mata_kuliah*$item->nilai_indeks;
+                $item['semester'] = $semester->semester;
 
                 return $item;
             });
@@ -140,7 +107,7 @@ class HistoriNilaiController extends Controller
             $nilai = null;
         }
         
-        $pdf = PDF::loadView('mahasiswa.histori_nilai.cetak', compact('riwayat_pendidikan', 'mahasiswa', 'nilai', 'periode'))->setPaper('a4', 'landscape');
+        $pdf = PDF::loadView('mahasiswa.histori_nilai.cetak', compact('riwayat_pendidikan', 'mahasiswa', 'nilai', 'periode'))->setPaper('a4');
         return $pdf->stream('Histori_-_Nilai-_-'.$mahasiswa->nama_mahasiswa.'.pdf');    
     }
 }
