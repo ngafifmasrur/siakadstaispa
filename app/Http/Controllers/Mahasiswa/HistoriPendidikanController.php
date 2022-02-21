2ce8bd9b01155e4022f87290f2c7683cc4e5ec9d
dwi <dwi@impstudio.id>
2022-02-22 02:18:47 +0700

    fix filter histori pendidikan

--- app/Http/Controllers/Mahasiswa/HistoriPendidikanController.php
+++ app/Http/Controllers/Mahasiswa/HistoriPendidikanController.php
@@ -30,7 +30,7 @@ class HistoriPendidikanController extends Controller
         $semester = m_global_konfigurasi::first()->value('id_semester_aktif');
 
         $query = t_riwayat_pendidikan_mahasiswa::setFilter([
-            'filter' => "id_mahasiswa='$user->id_mahasiswa' AND id_periode_masuk='$semester'"
+            'filter' => "id_mahasiswa='$user->id_mahasiswa'"
         ])->get();
 
         return datatables()->of($query)
