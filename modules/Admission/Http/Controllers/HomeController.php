2373d4f50a483bf7ce3470b2b50a85e44f8af1ee
dwi <dwi@impstudio.id>
2022-03-19 01:22:35 +0700

    Update cetak formulir dan keterangan diterima

--- modules/Admission/Http/Controllers/HomeController.php
+++ modules/Admission/Http/Controllers/HomeController.php
@@ -51,9 +51,115 @@ class HomeController extends Controller
     {
         $this->authorize('view', $registrant);
 
-        $pdf = \PDF::loadView('admission::home.form', compact('registrant'))
-                    ->setPaper('a4', 'portrait');
+        $user = $registrant->user;
+        $files = $registrant->admission->files->load([
+                        'registrants' => function($query) use ($registrant) { 
+                            $query->where('registrant_id', $registrant->id); 
+                        }
+                    ]);
+        $personal = [
+            'IDENTITAS' => [
+                'Nama lengkap' =>  $user->profile->full_name,
+                'Tempat, tanggal lahir' =>  $user->profile->pdob,
+                'NIK' =>  $user->profile->nik ?? null,
+                'No.KK' =>  $user->profile->nokk ?? null,
+                'NISN' =>  $user->profile->nisn ?? null,
+                'Jenis kelamin' =>  $user->profile->sex !== null ? config('web.references.sexes')[$user->profile->sex] : null,
+                'Golongan darah' =>  $user->profile->blood !== null ? config('web.references.bloods')[$user->profile->blood] : null,
+                'Kewarganegaraan' =>  $user->profile->country->name ?? null
+            ],
+            'KONTAK' => [
+                'No. Telp/HP' => $user->phone->number.($user->phone->whatsapp ? ' (WA)' : ''),
+                'Alamat e-mail' => $user->email->address,
+                'Alamat' => $user->address->branch,
+                'Regional' => $user->address->regional,
+                'Kode pos' => $user->address->postal,
+            ],
+            'LAINNYA' => [
+                'Jenis Pendaftaran' =>  $registrant->is_saman ? 'Santri Mandiri (SAMAN)' : 'REGULER',
+                'Tanggal ke pondok' =>  $registrant->tanggal_kedatangan ?? '-',
+            ],
+        ];
 
-        return $pdf->stream('FORMULIR-PENDAFTARAN-'.$registrant->kd.'.pdf');
+        $ttdparent = array_filter([
+            !$user->father->is_dead ? $user->father->name : null,
+            !$user->mother->is_dead ? $user->mother->name : null,
+        ]);
+
+        if($registrant->is_saman == 0) {
+            $parent = [
+                'INFORMASI AYAH' => [
+                    'NIK ayah' => $user->father->nik ?: null,
+                    'Nama ayah' => ($user->father->is_dead ? 'ALM. ' : '').$user->father->name ?? null,
+                    'Tempat, tanggal lahir' => $user->father->pdob ?? null,
+                    'Pendidikan terakhir' => $user->father->grade->name ?? null,
+                    'Pekerjaan' => $user->father->employment->name ?? null,
+                    'Penghasilan/bulan' => $user->father->salary->name ?? null,
+                ],
+                'INFORMASI IBU' => [
+                    'NIK ibu' => $user->mother->nik ?: null,
+                    'Nama ibu' => ($user->mother->is_dead ? 'ALM. ' : '').$user->mother->name ?? null,
+                    'Tempat, tanggal lahir' => $user->mother->pdob ?? null,
+                    'Pendidikan terakhir' => $user->mother->grade->name ?? null,
+                    'Pekerjaan' => $user->mother->employment->name ?? null,
+                    'Penghasilan/bulan' => $user->mother->salary->name ?? null,
+                ],
+                'PEMBAYARAN' => [
+                    'Biaya Pendidikan' => 'Rp. 1.700.000',
+                    'Biaya Pesantren' => 'Rp. 1.300.000',
+                    'Total' => 'Rp. 3.000.000',
+                    'Status Pembayaran' => $registrant->paid_off_at ? 'LUNAS' : 'Belum melakukan pembayaran / Belum Diverifikasi',
+                ],
+            ];
+        } else {
+            $parent = [
+                'INFORMASI AYAH' => [
+                    'NIK ayah' => $user->father->nik ?: null,
+                    'Nama ayah' => ($user->father->is_dead ? 'ALM. ' : '').$user->father->name ?? null,
+                    'Tempat, tanggal lahir' => $user->father->pdob ?? null,
+                    'Pendidikan terakhir' => $user->father->grade->name ?? null,
+                    'Pekerjaan' => $user->father->employment->name ?? null,
+                    'Penghasilan/bulan' => $user->father->salary->name ?? null,
+                ],
+                'INFORMASI IBU' => [
+                    'NIK ibu' => $user->mother->nik ?: null,
+                    'Nama ibu' => ($user->mother->is_dead ? 'ALM. ' : '').$user->mother->name ?? null,
+                    'Tempat, tanggal lahir' => $user->mother->pdob ?? null,
+                    'Pendidikan terakhir' => $user->mother->grade->name ?? null,
+                    'Pekerjaan' => $user->mother->employment->name ?? null,
+                    'Penghasilan/bulan' => $user->mother->salary->name ?? null,
+                ],
+            ];
+        }
+
+        $admission = [
+            'DETAIL PENDAFTARAN' => [
+                'Tahun' =>  $registrant->admission->period->name,
+                'Jalur pendaftaran' =>  $registrant->admission->name,
+                'Nama' =>  $user->profile->name,
+                'Tempat, tanggal lahir' =>  $user->profile->pdob,
+                'Nomor pendaftaran' =>  $registrant->kd,
+                'Tanggal tes' =>  $registrant->test_at ? strtoupper($registrant->test_at->formatLocalized('%A, %d %B %Y')) : null,
+                'Sesi tes' =>  $registrant->session->name. ' ('.$registrant->session->range.')',
+                'Terdaftar pada' =>  $registrant->created_at->formatLocalized('%d %B %Y pukul %H:%M'),
+            ]
+        ];
+
+        $cbts = $registrant->cbts->where('status', 2);
+
+        // Profile Photo
+        $gambar = $registrant->avatar ? "data:image/". pathinfo(public_path("storage/".$registrant->avatar), PATHINFO_EXTENSION) .";base64,".base64_encode(file_get_contents(public_path("storage/".$registrant->avatar))) : "data:image/". pathinfo(public_path('assets/img/img-blank-3-4.png'), PATHINFO_EXTENSION) .";base64,".base64_encode(file_get_contents(public_path('assets/img/img-blank-3-4.png')));
+        $pdf = \PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true])->loadView('admission::home.form', compact('registrant', 'user', 'files', 'personal', 'ttdparent', 'parent', 'admission', 'cbts', 'gambar'))
+                    ->setPaper('a4', 'portrait');
+        $pdf->getDomPDF()->setHttpContext(
+            stream_context_create([
+                'ssl' => [
+                    'allow_self_signed'=> TRUE,
+                    'verify_peer' => FALSE,
+                    'verify_peer_name' => FALSE,
+                ]
+            ])
+        );
+        return $pdf->download('FORMULIR-PENDAFTARAN-'.$registrant->kd.'.pdf');
     }
 }
--- modules/Admission/Resources/views/home/form.blade.php
+++ modules/Admission/Resources/views/home/form.blade.php
@@ -5,24 +5,13 @@
 @section('kop', 'PENERIMAAN MAHASISWA BARU '.$registrant->admission->period->name)
 @section('kop-sub', strtoupper($registrant->admission->period->instance->long_name))
 
-@php
-	
-	$user = $registrant->user;
-	$files = $registrant->admission->files->load([
-					'registrants' => function($query) use ($registrant) { 
-						$query->where('registrant_id', $registrant->id); 
-					}
-				]);
-
-@endphp
-
 @section('content')
 <main>
 	<div style="padding: 10px; border: 1px solid #ccc; border-radius: 10px; margin-bottom: 20px;">
 		<table>
 			<tr>
 				<td width="1" style="vertical-align: middle;">
-					<img class="rounded" src="{{ $registrant->avatar ? Storage::url($registrant->avatar) : asset('assets/img/img-blank-3-4.png') }}" style="height: 3cm;" align="center">
+					<img class="rounded" src="{{ $gambar }}" style="height: 3cm;" align="center">
 				</td>
 				<td style="padding-left: 15px; vertical-align: middle;">
 					<p class="muted" style="margin-bottom: 10px;">{{ $registrant->admission->full_name }} / {{ $registrant->kd }}</p>
@@ -32,58 +21,13 @@
 						<h2 style="margin-bottom: 5px;">{{ $user->profile->name }}</h2>
 					@endif
 					<p>{{ strtoupper($user->profile->pdob) }}</p>
-					<p>Pilihan program studi : <span style="padding-left: 10px;">1.</span> <strong>{{ $registrant->major1_name ?? '-' }}</strong> <span style="padding-left: 20px;">2.</span> <strong>{{ $registrant->major2_name ?? '-' }}</strong></p>
+					<p>Pilihan program studi : <span style="padding-left: 10px;"></span> <strong>{{ $registrant->major1_name ?? '-' }}</strong></p>
 				</td>
 			</tr>
 		</table>
 	</div>
 
     <div style="position:absolute; top: 160px;">
-        @php
-    		$personal = [
-    			'IDENTITAS' => [
-    				'Nama lengkap' =>  $user->profile->full_name,
-    				'Tempat, tanggal lahir' =>  $user->profile->pdob,
-    				'NIK' =>  $user->profile->nik ?? null,
-    				'No.KK' =>  $user->profile->nokk ?? null,
-    				'NISN' =>  $user->profile->nisn ?? null,
-    				'Jenis kelamin' =>  $user->profile->sex !== null ? config('web.references.sexes')[$user->profile->sex] : null,
-    				'Golongan darah' =>  $user->profile->blood !== null ? config('web.references.bloods')[$user->profile->blood] : null,
-    				'Kewarganegaraan' =>  $user->profile->country->name ?? null
-    			],
-    			'KONTAK' => [
-    				'No. Telp/HP' => $user->phone->number.($user->phone->whatsapp ? ' (WA)' : ''),
-    				'Alamat e-mail' => $user->email->address,
-    				'Alamat' => $user->address->branch,
-    				'Regional' => $user->address->regional,
-    				'Kode pos' => $user->address->postal,
-    			]
-    		];
-    
-    		$ttdparent = array_filter([
-    			!$user->father->is_dead ? $user->father->name : null,
-    			!$user->mother->is_dead ? $user->mother->name : null,
-    		]);
-    
-    		$parent = [
-    			'INFORMASI AYAH' => [
-    				'NIK ayah' => $user->father->nik ?: null,
-    				'Nama ayah' => ($user->father->is_dead ? 'ALM. ' : '').$user->father->name ?? null,
-    				'Tempat, tanggal lahir' => $user->father->pdob ?? null,
-    				'Pendidikan terakhir' => $user->father->grade->name ?? null,
-    				'Pekerjaan' => $user->father->employment->name ?? null,
-    				'Penghasilan/bulan' => $user->father->salary->name ?? null,
-    			],
-    			'INFORMASI IBU' => [
-    				'NIK ibu' => $user->mother->nik ?: null,
-    				'Nama ibu' => ($user->mother->is_dead ? 'ALM. ' : '').$user->mother->name ?? null,
-    				'Tempat, tanggal lahir' => $user->mother->pdob ?? null,
-    				'Pendidikan terakhir' => $user->mother->grade->name ?? null,
-    				'Pekerjaan' => $user->mother->employment->name ?? null,
-    				'Penghasilan/bulan' => $user->mother->salary->name ?? null,
-    			],
-    		];
-    	@endphp
     
     	<table style="height: 755px;">
     		<tr>
@@ -134,21 +78,29 @@
     				@endforeach
     				<hr style="margin: 20px 5px 30px 0">
     				<fieldset>
-    					<legend> RIWAYAT PENDIDIKAN</legend>
+    					<legend> NILAI TEST CBT</legend>
     					<br>
     					<table>
-    						@forelse($user->studies->take(4) as $study)
+								<tr>
+									<td style="padding:0 10px;">
+										<strong>Mapel</strong>
+									</td>
+									<td style="padding:0 10px;">
+										<strong>Skor</strong>
+									</td>
+								</tr>
+    						@forelse($cbts as $cbt)
     							<tr>
-    								<td nowrap style="padding:0 10px 0 0;" width="80">
-    									<strong>{{ $study->range }}</strong>
+    								<td style="padding:0 10px;">
+    									{{ $cbt->admission_cbt->mapel }}
     								</td>
     								<td style="padding:0 10px;">
-    									<strong>{{ $study->name }}</strong>
+    									{{ $cbt->total_skor }}
     								</td>
     							</tr>
     						@empty
     							<tr>
-    								<td class="muted"><i>Tidak ada riwayat pendidikan</i></td>
+    								<td class="muted"><i>Tidak test yang dikerjakan</i></td>
     							</p>
     						@endforelse
     					</table>
@@ -159,49 +111,8 @@
         <div class="center" style="position: absolute;bottom: 1.75cm; padding: 5px; border:1px solid #000">
             <p style="line-height: 6pt;"><small>PASTIKAN DATA TELAH TERISI DENGAN BENAR, SEMAKIN VALID DATA AKAN LEBIH MEMPERCEPAT PROSES SELEKSI MAHASISWA. <br> DATA DI ATAS MENJADI ACUAN UNTUK PEMBUATAN KTM SETELAH ANAK DINYATAKAN DITERIMA</small></p>
         </div>
-    	<div style="position: absolute;bottom: 3.5cm; padding-left: 10px;">
-    		<table width="100%">
-    			<tr>
-    				<td width="35%"></td>
-    				<td width="65%" class="center">
-    					<table>
-    						<tr>
-    							<td colspan="2">
-    								Dengan ini saya menyatakan sanggup hadir melaksanakan tes di lokasi pendaftaran sesuai waktu tes yang telah saya pilih,
-    							</td>
-    						</tr>
-    						<tr>
-    							<td width="50%">
-    								<div>&nbsp;</div>
-    								<div style="height: 100px; border-bottom: 1px dotted #000; margin: 0px 20px;">Orang tua/wali</div>
-    								({!! count($ttdparent) ? join('/', $ttdparent) : str_repeat('&nbsp;', 40) !!})
-    							</td>
-    							<td width="50%">
-    								<div>Sleman, {{ strftime('%d %B %Y') }}</div>
-    								<div style="height: 100px; border-bottom: 1px dotted #000; margin: 0px 20px;">Calon mahasiswa</div>
-    								({{ $user->profile->full_name }})
-    							</td>
-    						</tr>
-    					</table>
-    				</td>
-    			</tr>
-    		</table>
-    	</div>
+
     </div>
 </main>
 
-@php
-$admission = [
-	'DETAIL PENDAFTARAN' => [
-		'Tahun' =>  $registrant->admission->period->name,
-		'Jalur pendaftaran' =>  $registrant->admission->name,
-		'Nama' =>  $user->profile->name,
-		'Tempat, tanggal lahir' =>  $user->profile->pdob,
-		'Nomor pendaftaran' =>  $registrant->kd,
-		'Tanggal tes' =>  $registrant->test_at ? strtoupper($registrant->test_at->formatLocalized('%A, %d %B %Y')) : null,
-		'Sesi tes' =>  $registrant->session->name. ' ('.$registrant->session->range.')',
-		'Terdaftar pada' =>  $registrant->created_at->formatLocalized('%d %B %Y pukul %H:%M'),
-	]
-];
-@endphp
 @endsection
