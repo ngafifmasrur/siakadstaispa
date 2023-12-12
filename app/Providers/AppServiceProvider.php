bf408408aea2f3cff18aa5387429b29a8e1db92d
Dwi Kurnia Surya <dwi@impstudio.id>
2023-12-12 09:38:38 +0700

    refactor(home): show footer information

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Modules\Admission\Models\FooterInformation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,42 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        view()->composer('*', function ($view) {
+
+            /**
+             * Global variable yang bisa diakses di seluruh view blade
+             *
+             */
+
+            // Get active information
+            $activeInformation = $this->getActiveFooterInformation('informasi');
+
+            // Get active email
+            $activeEmail = $this->getActiveFooterInformation('email');
+
+            // Get active phone number
+            $activePhoneNumber = $this->getActiveFooterInformation('nomor telepon');
+
+            // Get active website
+            $activeWebsite = $this->getActiveFooterInformation('website');
+            /** @var \Illuminate\Contracts\View\View $view */
+            $view->with(compact('activeInformation', 'activeEmail', 'activePhoneNumber', 'activeWebsite'));
+        });
+
         setlocale(LC_TIME, 'id_ID');
+    }
+
+    /**
+     * Get active footer information based on type and status
+     *
+     * @param string $type
+     * @return mixed
+     */
+    function getActiveFooterInformation($type)
+    {
+        return FooterInformation::where([
+            'status' => true,
+            'type'   => $type
+        ])->first();
     }
 }
