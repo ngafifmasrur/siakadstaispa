38bc70e3341fa5c46d558c3fdf95766f20543401
Dwi Kurnia Surya <dwi@impstudio.id>
2023-12-12 09:40:58 +0700

    refactor(period): allow committee to update

--- modules/Admission/Resources/views/admin/database/manage/periode/create.blade.php
+++ modules/Admission/Resources/views/admin/database/manage/periode/create.blade.php
@@ -87,7 +87,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-7 offset-md-4">
                                 <button class="btn btn-success" type="submit">Simpan</button>
-                                <a class="btn btn-secondary" href="{{ request('next', url()->previous()) }}">Kembali</a>
+                                <a class="btn btn-secondary" href="{{ request('next', route('admission.admin.database.manage.periode.index')) }}">Kembali</a>
                             </div>
                         </div>
                     </form>
