16546098cb195463830f410e1b01b2dce8bd8ec1
Dwi Kurnia Surya <dwi@impstudio.id>
2024-01-09 11:37:04 +0700

    fest: cost information

---
+++ modules/Admission/Resources/views/admin/cost_information/edit.blade.php
@@ -0,0 +1,93 @@
+@extends('admission::admin.layouts.admin')
+
+@section('subtitle', 'Edit Biaya Pesantren - ')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item">
+        <a href="{{ route('admission.admin.cost_information.index') }}">Biaya Pesantren</a>
+    </li>
+    <li class="breadcrumb-item active">Edit Biaya Pesantren</li>
+@endsection
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h2 class="mb-0"><a class="text-decoration-none small" href="{{ request('next', url()->previous()) }}"><i
+                        class="mdi mdi-arrow-left-circle"></i></a> Ubah data Biaya Pesantren</h2>
+            <hr>
+            <div class="card">
+                <div class="card-body">
+                    <form class="form-block"
+                        action="{{ route('admission.admin.cost_information.update', [
+                            'costInformation' => $costInformation->id,
+                            'next' => request('next', url()->previous())
+                        ]) }}" method="POST" enctype="multipart/form-data">
+                        @csrf @method('PUT')
+
+                        <div class="form-group required row">
+                            <label class="col-md-4 col-form-label text-md-right">Nama</label>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control @error('detail') is-invalid @enderror "
+                                    name="detail" value="{{ old('detail', $costInformation->name) }}"
+                                    required autofocus>
+                                @error('detail')
+                                    <span class="invalid-feedback"> {{ $message }} </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group required row">
+                            <label class="col-md-4 col-form-label text-md-right">Detail</label>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control @error('detail') is-invalid @enderror "
+                                    name="detail" value="{{ old('detail', $costInformation->detail) }}"
+                                    required autofocus>
+                                @error('detail')
+                                    <span class="invalid-feedback"> {{ $message }} </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-7 offset-md-4">
+                                <button class="btn btn-success" type="submit">Simpan</button>
+                                <a class="btn btn-secondary" href="{{ request('next', url()->previous()) }}">Kembali</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('script')
+    <script>
+        $(document).ready(function() {
+
+            // Check the selected type on page load
+            if ($("#type").val() == "informasi") {
+                $(".content-default").hide();
+                $(".content-default input").prop('disabled', true);
+                $(".content-textarea").show();
+                $(".content-textarea textarea").prop('disabled', false);
+            }
+
+            // Update the content input to textarea on type change
+            $("#type").change(function() {
+                if ($(this).val() == "informasi") {
+                    $(".content-default").hide();
+                    $(".content-default input").prop('disabled', true);
+                    $(".content-textarea").show();
+                    $(".content-textarea textarea").prop('disabled', false);
+
+                } else {
+                    $(".content-default").show();
+                    $(".content-default input").prop('disabled', false);
+                    $(".content-textarea").hide();
+                    $(".content-textarea textarea").prop('disabled', true);
+
+                }
+            });
+        });
+    </script>
+@endpush
---
+++ modules/Admission/Resources/views/admin/cost_information/index.blade.php
@@ -0,0 +1,124 @@
+@extends('admission::admin.layouts.admin')
+
+@section('subtitle', 'Informasi Biaya - ')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item">Informasi Biaya</li>
+    <li class="breadcrumb-item active">Data Informasi Biaya</li>
+@endsection
+
+@section('section')
+    <div class="section">
+        <h3 class="mb-1">Kelola data Informasi Biaya</h3>
+        <div class="mb-2">Tambah dan ubah Informasi Biaya.</div>
+    </div>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="mb-0">Informasi Biaya</h5>
+                    <span class="text-muted">Menampilkan data Informasi Biaya</span>
+                    <br>
+                </div>
+                <div class="card-body bg-light border-top">
+                    <form id="constInformations"
+                        action="{{ route('admission.admin.cost_information.storeOrUpdateCosts') }}" method="POST">
+                        @csrf
+
+                        @foreach ($costInfomations as $costInfomation)
+                            <div class="form-group">
+                                <label for="{{ $costInfomation['key'] }}">{{ $costInfomation['name'] }}</label>
+                                <textarea class="form-control summernote"
+                                    name="{{ $costInfomation['key'] }}" id="{{ $costInfomation['key'] }}"
+                                    cols="5" rows="5">{{ $costInfomation['value'] }}</textarea>
+                            </div>
+                        @endforeach
+                    </form>
+
+                    <form class="form-block" id="search-form"
+                        action="{{ route('admission.admin.cost_information.index') }}"
+                        method="GET">
+                        <input type="hidden" name="limit" value="{{ request('l', 10) }}">
+                        <div class="form-inline d-flex justify-content-between">
+                            <h6>Biaya Pesantren</h6>
+                            <div class="my-1 mr-sm-2">
+                                <a class="btn btn-primary"
+                                    href="{{ route('admission.admin.cost_information.create', request()->all()) }}">
+                                    <i class="mdi mdi-plus"></i> Tambah Biaya Pesantren
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="table-responsive">
+                    <table class="table table-hover mb-0" aria-describedby="Cost Informations">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>Nama Biaya</th>
+                                <th>Detail</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($data as $item)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->detail }}</td>
+                                    <td class="py-2 align-middle border-left text-center" nowrap>
+                                        <a class="btn btn-warning btn-sm"
+                                            href="{{ route('admission.admin.cost_information.edit', [
+                                                'costInformation' => $item->id,
+                                            ]) }}">
+                                            <i class="mdi mdi-pencil"></i>
+                                        </a>
+                                        <form class="form-block form-confirm d-inline"
+                                            action="{{ route('admission.admin.cost_information.destroy', [
+                                                'costInformation' => $item->id,
+                                            ]) }}"
+                                            method="POST">
+                                            @csrf @method('DELETE')
+                                            <button class="btn btn-danger btn-sm">
+                                                <i class="mdi mdi-trash-can"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="3" class="text-muted px-4">
+                                        Tidak ada data Informasi Biaya
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="text-right">
+                <button type="button" class="btn btn-primary mb-3"
+                    onclick="document.getElementById('constInformations').submit();">
+                    Simpan
+                </button>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('style')
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+@endpush
+
+@push('script')
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+<script>
+    $('.summernote').summernote({
+      tabsize: 2,
+      height: 100
+    });
+  </script>
+@endpush
